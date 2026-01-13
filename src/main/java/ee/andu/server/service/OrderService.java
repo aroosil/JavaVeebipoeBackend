f9d15e903cf5fca09875ecf837c520448aa32ff3
aroosil <49617436+aroosil@users.noreply.github.com>
2026-01-13 19:59:36 +0200

    urls

--- src/main/java/ee/andu/server/service/OrderService.java
+++ src/main/java/ee/andu/server/service/OrderService.java
@@ -9,6 +9,7 @@ import ee.andu.server.repository.OrderRepository;
 import ee.andu.server.repository.PersonRepository;
 import ee.andu.server.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -36,6 +37,14 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Value("${everypay.url}")
+    private String baseUrl;
+
+    @Value("${everypay.customerURL}")
+    private String customerURL;
+
+    private String prefix = "asdafd";
+
     public double calculateCartSum(List<Product> products) {
         double sum = 0;
         for (Product product : products) {
@@ -61,16 +70,17 @@ public class OrderService {
     // success: order_reference=asdafd20&payment_reference=701d9f3bc2b86896b48b00f2eb9387c4e1f97204d77b269c6f8a3e2ae6927d05
     // fail: order_reference=asdafd21&payment_reference=1ac71a45979b5b5b48c72d4f06f960b1b328fd37650895780143eec5a6f35432
     public PaymentLink makePayment(Long order_reference, double total) {
-        String url = "https://igw-demo.every-pay.com/api/v4/payments/oneoff";
+        String requestUrl = baseUrl + "/payments/oneoff";
 
         EveryPayBody body = new EveryPayBody();
         body.setAccount_name("EUR3D1");
         body.setNonce("bla" + ZonedDateTime.now() + UUID.randomUUID());
         body.setTimestamp(ZonedDateTime.now().toString());
         body.setAmount(total);
-        body.setOrder_reference("asdafd" + order_reference);
-        body.setCustomer_url("https://err.ee");//"https://veebipood-frontend.web.app/");
-        body.setApi_username("e36eb40f5ec87fa2");
+        body.setOrder_reference(prefix + order_reference);
+
+        body.setCustomer_url(customerURL);
+        body.setApi_username("e36eb40f5ec87fa2"); // TODO: saab kui teha kasutaja antud firmale, kes lehte haldab
 
         HttpHeaders headers = new HttpHeaders();
         headers.setBasicAuth("e36eb40f5ec87fa2", "7b91a3b9e1b74524c2e9fc282f8ac8cd");
@@ -78,7 +88,7 @@ public class OrderService {
 
         HttpEntity<EveryPayBody> entity = new HttpEntity<>(body, headers);
 
-        EveryPayResponse response = restTemplate.exchange(url,  HttpMethod.POST, entity, EveryPayResponse.class).getBody();
+        EveryPayResponse response = restTemplate.exchange(requestUrl,  HttpMethod.POST, entity, EveryPayResponse.class).getBody();
 
         if (response == null || response.getPayment_link() == null || response.getPayment_link().isEmpty()) {
             throw new RuntimeException("Payment link is null or empty");
@@ -102,7 +112,7 @@ public class OrderService {
     public OrderPaid checkPayment(String orderReference, String paymentReference) {
         String debug_username = "e36eb40f5ec87fa2";
 
-        String url = "https://igw-demo.every-pay.com/api/v4/payments/" + paymentReference + "?api_username=" + debug_username + "&detailed=false";
+        String requestUrl = baseUrl + "/" + paymentReference + "?api_username=" + debug_username + "&detailed=false";
 //        String url = "https://igw-demo.every-pay.com/api/v4/payments/701d9f3bc2b86896b48b00f2eb9387c4e1f97204d77b269c6f8a3e2ae6927d05?api_username=e36eb40f5ec87fa2&detailed=false";
 
         HttpHeaders headers = new HttpHeaders();
@@ -111,7 +121,7 @@ public class OrderService {
 
         HttpEntity<EveryPayBody> entity = new HttpEntity<>(null, headers);
 
-        EveryPayStatus response = restTemplate.exchange(url,  HttpMethod.GET, entity, EveryPayStatus.class).getBody();
+        EveryPayStatus response = restTemplate.exchange(requestUrl,  HttpMethod.GET, entity, EveryPayStatus.class).getBody();
 
         if (response == null ) {
             throw new RuntimeException("Could not get payment status");
@@ -122,7 +132,7 @@ public class OrderService {
 
         PaymentState paymentState = (PaymentState.valueOf(response.getPayment_state().toUpperCase()));
 
-        String order_id = response.getOrder_reference().replace("asdafd", "");
+        String order_id = response.getOrder_reference().replace(prefix, "");
         Order order = orderRepository.findById(Long.parseLong(order_id)).orElseThrow();
         order.setPaymentState(paymentState);
 
